3eab70a0df77a1540ad576386ed2d1688ca40f6b
stefaanmeeuws <stefaan.meeuws@outlook.com>
2021-10-29 23:39:03 +0200

    Added method comments.

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,11 +1,11 @@
 public class AreaCalculator {
-    public static double area(double radius) {
+    public static double area(double radius) { // calculates the area of a circle
         if (radius < 0)
             return -1;
         return radius * radius * Math.PI;
     }
 
-    public static double area(double x, double y) {
+    public static double area(double x, double y) { // calculates the area of a rectangle
         /* This "if" and the following "if" whould be joined by two PIPE characters, but I can't find them on my laptop keyboard. */
         if (x < 0)
             return -1;
